8313f9b7414256af0867e8c81e85d8674a11c368
wwq <wwquen3568@naver.com>
2024-01-03 20:10:41 +0900

    (init) Initialize

---
+++ problem_1.py
@@ -0,0 +1,18 @@
+
+def problem_1():
+    """
+    코드 작성
+    """
+    pass
+
+def problem_2(phone_book):
+    answer = True
+    """
+    코드 작성
+    """
+    return answer
+    
+if __name__ == '__main__':
+    problem_1()
+    problem_2(["12","123","1235","567","88"])
+    problem_2(["123","456","789"])
